22e4d52dd68e417f3c7f12c90e60fda954c5a370
ft.gayashan@gmail.com <ft.gayashan@gmail.com>
2025-06-30 07:20:12 +0530

    Initial commit

---
+++ src/main/java/com/project/schedule_task/ScheduleTask.java
@@ -0,0 +1,33 @@
+package com.project.schedule_task;
+
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Service;
+
+import java.util.Date;
+
+@Service
+public class ScheduleTask {
+
+//    @Scheduled(fixedRate = 5000) // 5S
+//    public void runEveryFiveSeconds(){
+//        System.out.println("Fixed Rate Task: " + new Date());
+//    }
+//
+//    @Scheduled(fixedDelay = 6000)
+//    public void runWithDelay(){
+//        System.out.println("Run with delay : " + new Date());
+//    }
+
+//    @Scheduled(initialDelay = 7000, fixedRate = 10000)
+//    public void runWithInitialDelay(){
+//        System.out.println("Run with initial delay : " + new Date());
+//    }
+
+//    "* * * * * *"
+
+    @Scheduled(cron = "")
+    public void testCronTask(){
+        System.out.println("Current time : " + new Date());
+    }
+
+}
