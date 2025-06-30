5be1e9295ff2b04ce849319ede7f19eaa4383413
ft.gayashan@gmail.com <ft.gayashan@gmail.com>
2025-06-30 07:21:43 +0530

    initial commit

--- src/main/java/com/project/schedule_task/ScheduleTask.java
+++ src/main/java/com/project/schedule_task/ScheduleTask.java
@@ -8,22 +8,20 @@ import java.util.Date;
 @Service
 public class ScheduleTask {
 
-//    @Scheduled(fixedRate = 5000) // 5S
-//    public void runEveryFiveSeconds(){
-//        System.out.println("Fixed Rate Task: " + new Date());
-//    }
-//
-//    @Scheduled(fixedDelay = 6000)
-//    public void runWithDelay(){
-//        System.out.println("Run with delay : " + new Date());
-//    }
+    @Scheduled(fixedRate = 5000) // 5S
+    public void runEveryFiveSeconds(){
+        System.out.println("Fixed Rate Task: " + new Date());
+    }
 
-//    @Scheduled(initialDelay = 7000, fixedRate = 10000)
-//    public void runWithInitialDelay(){
-//        System.out.println("Run with initial delay : " + new Date());
-//    }
+    @Scheduled(fixedDelay = 6000)
+    public void runWithDelay(){
+        System.out.println("Run with delay : " + new Date());
+    }
 
-//    "* * * * * *"
+    @Scheduled(initialDelay = 7000, fixedRate = 10000)
+    public void runWithInitialDelay(){
+        System.out.println("Run with initial delay : " + new Date());
+    }
 
     @Scheduled(cron = "")
     public void testCronTask(){
